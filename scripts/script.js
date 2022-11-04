684d36262c5870fd030aefa8dd063a88ccd5a5f7
menarselamet <menarselamet007@gmail.com>
2022-11-05 00:34:11 +0300

    adding eventlistener to buttons

--- scripts/script.js
+++ scripts/script.js
@@ -3,18 +3,25 @@ const arr = [];
 fetch(`https://random-word-api.herokuapp.com/word?number=1`)
   .then((res) => res.json())
   .then((data) => fetchDamnData(data));
-function fetchDamnData(damnData) {
+function fetchDamnData(damnData){
   const word = damnData[0].split("");
-  arr.push(word);
-}
+  arr.push(word);}
 console.log(arr);
+
+
 function generateAlphabet() {
   let alphabet = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
-  alphabet.split(" ").map((letter) => {
+  alphabet.toUpperCase().split(" ").map((letter) => {
     const btns = document.querySelector(".btns");
     const buttons = document.createElement("button");
+    buttons.setAttribute('class', 'buttonBoxes');
     buttons.innerText = letter;
-    btns.appendChild(buttons);
-  });
-}
+    btns.appendChild(buttons);});}
 generateAlphabet();
+
+document.addEventListener("click", (e) => {
+  if (e.target.className === "buttonBoxes"){
+      e.target.classList.add("clicked");
+    let theClickedLetter = e.target.innerText;
+    console.log(theClickedLetter);
+    }})
